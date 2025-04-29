7eb5e78a182d824c511092b59d495f3fd046a0ac
Abdulrahman Omar <109946184+aaomerali@users.noreply.github.com>
2025-04-29 11:52:52 +0300

    build navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../features/auth/authSlice';
 
 function Navbar() {
+  const { isAuthenticated } = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+
   return (
-    <div>Navbar</div>
-  )
+    <nav className="bg-gray-800 text-white p-4 mb-10">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-xl font-bold">LinkTreeClone</Link>
+        
+        <div className="space-x-4">
+          {isAuthenticated ? (
+            <>
+              <Link to="/dashboard" className="hover:text-gray-300">Dashboard</Link>
+              <button onClick={() => dispatch(logout())} className="hover:text-gray-300">
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to="/login" className="hover:text-gray-300">Login</Link>
+              <Link to="/register" className="hover:text-gray-300">Register</Link>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar
